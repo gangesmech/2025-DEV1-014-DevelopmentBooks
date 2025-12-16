10cb8a15294434110107cdf0e21b099f65b5df3f
Raja kanganathan <rajakanganathan@gmail.com>
2025-12-16 16:49:46 +0530

    test: apply correct discounts for 3, 4 and 5 distinct books

--- src/test/java/com/bnppf/kata/developmentbooks/ShoppingBasketTest.java
+++ src/test/java/com/bnppf/kata/developmentbooks/ShoppingBasketTest.java
@@ -1,11 +1,24 @@
 package com.bnppf.kata.developmentbooks;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 class ShoppingBasketTest {
 
     private static final double BOOK_PRICE = 50.0;
+    private static final String[] BOOKS = {
+            "Clean Code",
+            "The Clean Coder",
+            "Clean Architecture",
+            "Test Driven Development by Example",
+            "Working Effectively with Legacy Code"
+    };
 
     @Test
     void emptyBasketDescription() {
@@ -16,15 +29,32 @@ class ShoppingBasketTest {
     @Test
     void singleBookCostsFifty() {
         var basket = new ShoppingBasket();
-        basket.add(new Book("Clean Code"));
+        basket.add(new Book(BOOKS[0]));
         assertEquals(BOOK_PRICE, basket.calculate());
     }
 
     @Test
     void twoDifferentBooksDiscount() {
         var basket = new ShoppingBasket();
-        basket.add(new Book("Clean Code"));
-        basket.add(new Book("The Clean Coder"));
+        basket.add(new Book(BOOKS[0]));
+        basket.add(new Book(BOOKS[1]));
         assertEquals(95.0, basket.calculate());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideBookCountsAndExpectedPrices")
+    void distinctBooksReceiveDiscounts(int distinctBooksCount, double expectedPrice) {
+        var basket = new ShoppingBasket();
+        IntStream.range(0, distinctBooksCount)
+                .mapToObj(i -> new Book(BOOKS[i]))
+                .forEach(basket::add);
+        assertEquals(expectedPrice, basket.calculate());
+    }
+
+    static Stream<Arguments> provideBookCountsAndExpectedPrices() {
+        return Stream.of(
+                of(3, 135.0),
+                of(4, 160.0),
+                of(5, 187.5));
     }
 }
