d61c0414a530bc76b056fdbbccda3d0630de9e9d
Raja kanganathan <rajakanganathan@gmail.com>
2025-12-16 15:14:00 +0530

    chore: initialize Spring Boot project with Java and JUnit 5

---
+++ src/test/java/com/bnppf/kata/developmentbooks/DevelopmentBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnppf.kata.developmentbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
