9ebd677fb8192d9f433788c2af3b85444ec44584
Raja kanganathan <rajakanganathan@gmail.com>
2025-12-16 16:00:27 +0530

    test: single book should cost 50 EUR

--- src/test/java/com/bnppf/kata/developmentbooks/ShoppingBasketTest.java
+++ src/test/java/com/bnppf/kata/developmentbooks/ShoppingBasketTest.java
@@ -5,9 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ShoppingBasketTest {
 
+    private static final double BOOK_PRICE = 50.0;
+
     @Test
     void emptyBasketDescription() {
         var basket = new ShoppingBasket();
         assertEquals(0.0, basket.calculate());
+    }
+
+    @Test
+    void singleBookCostsFifty() {
+        var basket = new ShoppingBasket();
+        basket.add(new Book("Clean Code"));
+        assertEquals(BOOK_PRICE, basket.calculate());
+    }
+
+    record Book(String title) {
     }
 }
