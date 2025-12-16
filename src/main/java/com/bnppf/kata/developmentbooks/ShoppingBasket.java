6711f608aed7e1c7b2208cb1910bd724c56fcf32
Raja kanganathan <rajakanganathan@gmail.com>
2025-12-16 19:44:54 +0530

    refactor: model basket as book frequency map

--- src/main/java/com/bnppf/kata/developmentbooks/ShoppingBasket.java
+++ src/main/java/com/bnppf/kata/developmentbooks/ShoppingBasket.java
@@ -1,11 +1,10 @@
 package com.bnppf.kata.developmentbooks;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ShoppingBasket {
-    private final List<Book> books = new ArrayList<>();
+    private final Map<Book, Integer> bookQuantities = new HashMap<>();
     private static final double BOOK_PRICE = 50.0;
     private static final Map<Integer, Double> DISCOUNTS = Map.of(
             2, 0.05,
@@ -14,14 +13,15 @@ public class ShoppingBasket {
             5, 0.25);
 
     public void add(Book book) {
-        books.add(book);
+        bookQuantities.merge(book, 1, Integer::sum);
     }
 
     public double calculate() {
-        var distinctBooksCount = (int) books.stream().map(Book::title).distinct().count();
+        var distinctBooksCount = bookQuantities.size();
         var discount = DISCOUNTS.getOrDefault(distinctBooksCount, 0.0);
         var discountedPrice = distinctBooksCount * BOOK_PRICE * (1.0 - discount);
-        var remainingBooksCount = books.size() - distinctBooksCount;
+        var totalBooksCount = bookQuantities.values().stream().mapToInt(Integer::intValue).sum();
+        var remainingBooksCount = totalBooksCount - distinctBooksCount;
         return discountedPrice + (remainingBooksCount * BOOK_PRICE);
     }
 }
