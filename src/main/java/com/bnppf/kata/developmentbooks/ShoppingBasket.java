c9cc9951cf2000b60a6a970a0b167e3cf4640948
Raja kanganathan <rajakanganathan@gmail.com>
2025-12-16 16:54:49 +0530

    feat: calculate price using discount table

--- src/main/java/com/bnppf/kata/developmentbooks/ShoppingBasket.java
+++ src/main/java/com/bnppf/kata/developmentbooks/ShoppingBasket.java
@@ -6,7 +6,11 @@ import java.util.Map;
 
 public class ShoppingBasket {
     private final List<Book> books = new ArrayList<>();
-    private static final Map<Integer, Double> DISCOUNTS = Map.of(2, 0.05);
+    private static final Map<Integer, Double> DISCOUNTS = Map.of(
+            2, 0.05,
+            3, 0.10,
+            4, 0.20,
+            5, 0.25);
 
     public void add(Book book) {
         books.add(book);
